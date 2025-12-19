d2a65c772e9d392a02eaf7db9865ff7b28e881f5
RobloxScriptsV2 <mernaif3@gmail.com>
2025-12-20 05:55:04 +0800

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://gitlab.com/rlbx-scripts/elysium/-/raw/main/adm-visual"))()
-
 loadstring(game:HttpGet("https://pastefy.app/k0GzYEfQ/raw"))()
+
+loadstring(game:HttpGet("https://gitlab.com/rlbx-scripts/elysium/-/raw/main/adm-visual"))()
